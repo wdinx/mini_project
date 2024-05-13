2cfd6d383263dec7a181e6a406eb6c9da26ef8c8
wdinx <wahyuudin2811@gmail.com>
2024-05-13 14:19:22 +0800

    Improved column data types in payment and transaction tables

--- model/domain/payment.go
+++ model/domain/payment.go
@@ -3,7 +3,7 @@ package domain
 import "github.com/google/uuid"
 
 type Payment struct {
-	ID            uuid.UUID   `gorm:"column:id;primaryKey;not null;type:varchar"`
+	ID            uuid.UUID   `gorm:"column:id;type:varchar(255);primaryKey;not null;"`
 	TransactionID uuid.UUID   `gorm:"column:transaction_id;not null"`
 	Transaction   Transaction `gorm:"foreignKey:transaction_id;references:id"`
 	Amount        float64     `gorm:"column:amount;not null"`
